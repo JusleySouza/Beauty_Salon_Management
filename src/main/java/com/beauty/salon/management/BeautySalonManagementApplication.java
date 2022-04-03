86e1c5fc416813dab794621b23e59db2e78e7b93
Jusley Souza <jusleysouza31@gmail.com>
2022-04-03 00:08:55 -0300

    Inicialização do Projeto

---
+++ src/main/java/com/beauty/salon/management/BeautySalonManagementApplication.java
@@ -0,0 +1,13 @@
+package com.beauty.salon.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySalonManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautySalonManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beauty/salon/management/BeautySalonManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beauty.salon.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautySalonManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
