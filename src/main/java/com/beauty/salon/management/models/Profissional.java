061f9d0c195cbaab3aa33be5eeb8a54024906820
Jusley Souza <jusleysouza31@gmail.com>
2022-04-03 01:35:05 -0300

    Criação do model Profissional

---
+++ src/main/java/com/beauty/salon/management/models/Profissional.java
@@ -0,0 +1,64 @@
+package com.beauty.salon.management.models;
+
+public class Profissional {
+
+	private String nome;
+	private String telefone;
+	private Endereco endereco;
+	private String email;
+	private String cpf;
+	private String cargo;
+	private String dataNascimento;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	@Override
+	public String toString() {
+		return "Profissional [nome=" + nome + ", telefone=" + telefone + ", endereco=" + endereco + ", email=" + email
+				+ ", cpf=" + cpf + ", cargo=" + cargo + ", dataNascimento=" + dataNascimento + "]";
+	}
+	
+	
+}
