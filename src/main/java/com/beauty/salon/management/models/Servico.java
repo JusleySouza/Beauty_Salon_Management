7b369edef8cfa5fb4917b18ba3428d294dd26e6b
Jusley Souza <jusleysouza31@gmail.com>
2022-04-03 02:12:13 -0300

    Adição dos atributos horas e minutos estimados

--- src/main/java/com/beauty/salon/management/models/Servico.java
+++ src/main/java/com/beauty/salon/management/models/Servico.java
@@ -5,8 +5,22 @@ public class Servico {
 	private String tipoServico;
 	private String nome;
 	private double valor;
+	private int horasEstimadas;
+	private int minutosEstimados;
 	
 	
+	public int getHorasEstimadas() {
+		return horasEstimadas;
+	}
+	public void setHorasEstimadas(int horasEstimadas) {
+		this.horasEstimadas = horasEstimadas;
+	}
+	public int getMinutosEstimados() {
+		return minutosEstimados;
+	}
+	public void setMinutosEstimados(int minutosEstimados) {
+		this.minutosEstimados = minutosEstimados;
+	}
 	public String getTipoServico() {
 		return tipoServico;
 	}
@@ -28,8 +42,12 @@ public class Servico {
 	
 	@Override
 	public String toString() {
-		return "Servico [tipoServico=" + tipoServico + ", nome=" + nome + ", valor=" + valor + "]";
+		return "Servico [tipoServico=" + tipoServico + ", nome=" + nome + ", valor=" + valor + ", horasEstimadas="
+				+ horasEstimadas + ", minutosEstimados=" + minutosEstimados + "]";
 	}
 	
 	
-}
+	}
+	
+	
+
