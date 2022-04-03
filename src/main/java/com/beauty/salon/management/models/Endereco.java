f88d1087824d76a4e5f419691f8f40f037e766b8
Jusley Souza <jusleysouza31@gmail.com>
2022-04-03 01:33:30 -0300

    Criação do model Endereço

---
+++ src/main/java/com/beauty/salon/management/models/Endereco.java
@@ -0,0 +1,59 @@
+package com.beauty.salon.management.models;
+
+import java.util.UUID;
+
+public class Endereco {
+
+	private UUID id;
+	private String rua;
+	private int numero;
+	private String bairro;
+	private String cidade;
+	private String cep;
+	
+	
+	public UUID getId() {
+		return id;
+	}
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	
+	@Override
+	public String toString() {
+		return "Endereco [id=" + id + ", rua=" + rua + ", numero=" + numero + ", bairro=" + bairro + ", cidade="
+				+ cidade + ", cep=" + cep + "]";
+	}
+	
+	
+}
