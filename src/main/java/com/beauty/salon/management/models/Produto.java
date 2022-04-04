b0eec65c2c44b1e7ad5e5cbe176feb443c9dc639
Jusley Souza <jusleysouza31@gmail.com>
2022-04-04 00:30:08 -0300

    Mapeamento do modelo de produtos

--- src/main/java/com/beauty/salon/management/models/Produto.java
+++ src/main/java/com/beauty/salon/management/models/Produto.java
@@ -1,5 +1,72 @@
 package com.beauty.salon.management.models;
 
-public class Produto {
+import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="produtos")
+public class Produto implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String lote;
+	private double valor;
+	private Date dataCompra;
+	private Date dataVencimento;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLote() {
+		return lote;
+	}
+	public void setLote(String lote) {
+		this.lote = lote;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	public Date getDataCompra() {
+		return dataCompra;
+	}
+	public void setDataCompra(Date dataCompra) {
+		this.dataCompra = dataCompra;
+	}
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+	
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", nome=" + nome + ", lote=" + lote + ", valor=" + valor + ", dataCompra="
+				+ dataCompra + ", dataVencimento=" + dataVencimento + "]";
+	}
+	
+	
 }
